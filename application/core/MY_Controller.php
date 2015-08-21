7cfc87e29e00ee9d45c3e51f259e15d91f4c7f17
zhouhui <95459910@qq.com>
2015-08-21 10:49:27 +0800

    zhouhui

---
+++ application/core/MY_Controller.php
@@ -0,0 +1,26 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 2015/8/20
+ * Time: 17:11
+ */
+
+class MY_Controller extends CI_Controller{
+    public function __construct(){
+        parent::__construct();
+    }
+}
+//后台控制器
+class AdminController extends MY_Controller
+{
+    public function __construct()
+    {
+        parent::__construct();
+
+        if(empty($_SESSION['admin_name'])) {
+            echo '必须登录';exit;
+        }
+    }
+}
