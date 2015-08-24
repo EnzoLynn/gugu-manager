28105dab7140766ec508204ce99b53c0ec540f91
zhouhui <95459910@qq.com>
2015-08-24 18:12:22 +0800

    zhouhui

--- application/controllers/Customer.php
+++ application/controllers/Customer.php
@@ -11,6 +11,25 @@ class Customer extends AdminController {
     public function getList(){
         $this->load->model('customer');
 
+        $data = array(
+            'page' => int($this->input->post('page')),
+            'limit'=> int($this->input->post('limit')),
+            'sort' => $this->input->post('sort'),
+            'dir'  => $this->input->post('dir'),
+            'filter' => json_decode($this->input->post('filter'))
+        );
+        $customers = $this->customer_model->getCustomers($data);
+
+        $customer_total = $this->customer_model->getCustomersTotal($data);
+
+        $json = array(
+            'success' => true,
+            'data' => $customers,
+            'total' => $customer_total,
+            'msg' => '成功',
+            'code' => '01'
+        );
+        echo json_encode($json);
     }
 
     public function add() {
