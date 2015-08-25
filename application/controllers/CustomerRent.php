ad74350d977cb1f25f865abfd69270ce762406ee
zhouhui <95459910@qq.com>
2015-08-25 16:21:30 +0800

    zhouhui

--- application/controllers/CustomerRent.php
+++ application/controllers/CustomerRent.php
@@ -1,36 +1,50 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-class Customer extends AdminController {
+class CustomerRent extends AdminController {
 
-    public function index()
-    {
-        //$this->load->view('login');
+    public function index(){
+
     }
 
     public function getList(){
-        $admin_name = $this->input->post('admin_name');
-        $admin_pwd = $this->input->post('admin_pwd');
-        $admin = $this->admin_model->login($admin_name, $admin_pwd);
+        $this->load->model('customer_rent_model');
 
-        if($admin) {
-            $this->input->set_cookie('login_sessiontoken', session_id(), 60*60*24);
-            $this->input->set_cookie('login_username',$admin['admin_name'], 60*60*24);
+        $data = array(
+            'page' => (int)$this->input->post('page'),
+            'limit'=> (int)$this->input->post('limit'),
+            'sort' => $this->input->post('sort'),
+            'dir'  => $this->input->post('dir'),
+            'filter' => json_decode($this->input->post('filter')),
+            'customer_id' => (int)$this->input->post(customer_id)
+        );
+        $customer_rents = $this->customer_rent_model->getCustomerRents($data);
 
-            $json = array(
-                'success' => true,
-                'data' => $admin,
-                'total' => 1,
-                'msg' => '登录成功',
-                'code' => '01'
-            );
-        }else {
-            $json = array(
-                'success' => false,
-                'msg' => '帐号或者密码错误'
-            );
-        }
-        //echo '<pre>';print_r($json);exit;
-        return urldecode(json_encode($json));
+        $customer_rents_total = $this->customer_rent_model->getCustomerRentsTotal($data);
+
+        $json = array(
+            'success' => true,
+            'data' => $customer_rents,
+            'total' => $customer_rents_total,
+            'msg' => '成功',
+            'code' => '01'
+        );
+        echo json_encode($json);
+    }
+
+    public function add() {
+        $this->load->model('customer_rent_model');
+
+        $data = array(
+            'customer_id' => $this->input->post('customer_id'),
+            'title' => $this->input->post('title'),
+            'rent_area' => $this->input->post('rent_area'),
+            'area_to_order_number' => $this->input->post('area_to_order_number'),
+            'rent_pre_price' => $this->input->post('rent_pre_price'),
+            'date_start' => $this->input->post('date_start'),
+            'date_end' => $this->input->post('date_end')
+        );
+
+        $this->customer_model->addCustomer($data);
     }
 }
