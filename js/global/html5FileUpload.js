2859d380aff1eeeda129aa61c2bb4d96c32c2ac2
chenheling <6710582@qq.com>
2015-08-27 17:54:56 +0800

    html5 upload

--- js/global/html5FileUpload.js
+++ js/global/html5FileUpload.js
@@ -60,7 +60,7 @@
 
          scope.updatePropress(xhr, scope);
 
-         xhr.open("POST", scope.uploadUrl,true);
+         xhr.open("POST", scope.uploadUrl, true);
          //xhr.overrideMimeType('text/plain; charset=x-user-defined-binary');
          // reader.onload = function(evt) {
          //     xhr.send(evt.target.result);
@@ -141,7 +141,7 @@
          dropzone.ondrop = function(event) {
              event.stopPropagation();
              event.preventDefault();
-             if (Ext.isIe || (Ext.isGecko && Ext.firefoxVersion < 30) || (Ext.isWebKit && Ext.chromeVersion < 30)) {
+             if ((Ext.isIe && Ext.ieVersion < 10) || (Ext.isGecko && Ext.firefoxVersion < 30) || (Ext.isWebKit && Ext.chromeVersion < 30)) {
                  Ext.Msg.alert('消息', '您的浏览器不支持Html5上传,请更换浏览器或升级版本。');
                  return;
              }
