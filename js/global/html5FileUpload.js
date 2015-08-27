7bcdc1eca7586b5466893cba03caec5f7cb3dd84
chenheling <6710582@qq.com>
2015-08-27 15:40:43 +0800

    finish

--- js/global/html5FileUpload.js
+++ js/global/html5FileUpload.js
@@ -3,9 +3,10 @@
      alias: 'widget.Html5FileUpload',
      accept: '*/*',
      style: '',
-     progressContainerEl:'',
+     uploadUrl: '',
+     progressContainerEl: '',
      progressEl: '',
-     countEl:'',
+     countEl: '',
      onRender: function() {
          var me = this,
              inputEl;
@@ -24,6 +25,77 @@
          me.browseButtonWrap.dom.style.width = (me.browseButtonWrap.dom.lastChild.offsetWidth + me.buttonEl.getMargin('lr')) + 'px';
          if (Ext.isIE) {
              me.buttonEl.repaint();
+         }
+     },
+     sendFile: function(file,scope) {
+         var me = this;
+         var xhr = new XMLHttpRequest();
+         var fd = new FormData();
+         this.xhr = xhr;
+
+         var self = this;
+         this.xhr.upload.addEventListener("progress", function(e) {
+
+             if (e.lengthComputable) {
+                 var percentage = Math.round((e.loaded * 100) / e.total);
+                 console.log(percentage);
+                 scope.progressEl.dom.value = percentage;
+             }
+         }, false);
+
+         xhr.upload.addEventListener("load", function(e) {
+             console.log(100);
+             scope.progressEl.dom.value = 100;
+
+         }, false);
+         xhr.open("POST", me.uploadUrl, true);
+         xhr.onreadystatechange = function() {
+             if (xhr.readyState == 4 && xhr.status == 200) {
+                 // Handle response.
+                 console.log(xhr.responseText); // handle response.
+             }
+         };
+
+         fd.append('myFile', file);
+         // Initiate a multipart/form-data upload
+         xhr.send(fd);
+     },
+     FileUpload: function(file, scope) {
+
+         var reader = new FileReader();
+
+         var xhr = new XMLHttpRequest();
+         this.xhr = xhr;
+
+         var self = this;
+         this.xhr.upload.addEventListener("progress", function(e) {
+
+             if (e.lengthComputable) {
+                 var percentage = Math.round((e.loaded * 100) / e.total);
+                 console.log(percentage);
+                 scope.progressEl.dom.value = percentage;
+             }
+         }, false);
+
+         xhr.upload.addEventListener("load", function(e) {
+             console.log(100);
+             scope.progressEl.dom.value = 100;
+
+         }, false);
+         xhr.open("POST", scope.uploadUrl);
+         xhr.overrideMimeType('text/plain; charset=x-user-defined-binary');
+         reader.onload = function(evt) {
+             xhr.send(evt.target.result);
+         };
+         reader.readAsBinaryString(file);
+
+
+     },
+
+     sendFiles: function(files) {
+         var me = this;
+         for (var i = 0; i < files.length; i++) {
+             new me.FileUpload(files[i], me);
          }
      },
      createFileInput: function() {
@@ -45,7 +117,7 @@
              change: me.onFileChange
          });
 
-         var dropzone = me.fileInputEl.dom;
+         var dropzone = me.el.dom;
          dropzone.ondragover = dropzone.ondragenter = function(event) {
              event.stopPropagation();
              event.preventDefault();
@@ -57,32 +129,33 @@
 
              var filesArray = event.dataTransfer.files;
              for (var i = 0; i < filesArray.length; i++) {
-                 sendFile(filesArray[i]);
+                 me.sendFile(filesArray[i],me);
              }
          }
 
          me.progressContainerEl = me.triggerWrap.up('#' + me.id).createChild({
              name: me.getName(),
-             width: me.getWidth(), 
+             style: "width:" + me.getWidth() + "px;",
              id: me.id + '-fileInputProgressContainerEl',
              cls: Ext.baseCSSPrefix + 'form-file-input-progressContainer',
              tag: 'div'
-         }); 
+         });
          me.progressEl = me.progressContainerEl.createChild({
              name: me.getName(),
-             width: me.getWidth(),
+             style: "width:" + (me.getWidth() - 120) + "px;",
              value: 0,
+             max: 100,
              id: me.id + '-fileInputProgressEl',
              cls: Ext.baseCSSPrefix + 'form-file-input-progress',
              tag: 'progress'
-         }); 
+         });
          me.countEl = me.progressContainerEl.createChild({
              name: me.getName(),
-             width: me.getWidth(), 
+             style: "width:100px;margin-left:5px;",
              id: me.id + '-fileInputCountEl',
              cls: Ext.baseCSSPrefix + 'form-file-input-count',
              tag: 'label'
-         }); 
-         me.countEl.dom.innerHTML="0";
+         });
+         me.countEl.dom.innerHTML = "文件:0/0";
      }
  });
