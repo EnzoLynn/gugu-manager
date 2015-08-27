b0a98ff0698e87106a65438ab78ee7035ed490f7
chenheling <6710582@qq.com>
2015-08-27 17:34:06 +0800

    html5 upload

--- js/global/html5FileUpload.js
+++ js/global/html5FileUpload.js
@@ -148,7 +148,7 @@
 
 
 
-
+             var files = event.dataTransfer.files;
 
              var str = "";
              for (var i = 0; i < files.length; i++) {
@@ -168,7 +168,7 @@
                  Ext.Msg.alert('添加文件', '不支持的文件格式！');
                  return;
              }
-             var files = event.dataTransfer.files;
+
              me.progressEl.dom.value = 0;
              me.countEl.dom.innerHTML = "文件: 1" + '/' + files.length;
              me.totalFile = Ext.clone(files.length);
