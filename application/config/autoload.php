36c4ffe22db7835a268dc14ba4a65b74c41f70fe
zhouhui <95459910@qq.com>
2015-08-27 09:50:07 +0800

    zhouhui

--- application/config/autoload.php
+++ application/config/autoload.php
@@ -42,7 +42,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 $autoload['packages'] = array();
 
-$autoload['libraries'] = array('database');//'session'
+$autoload['libraries'] = array('database', 'session');
 /*
 | -------------------------------------------------------------------
 |  Auto-load Libraries
