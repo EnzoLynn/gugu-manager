130c3dfe78936d0614041914c769b6652262c0f6
chenheling <6710582@qq.com>
2015-09-11 12:06:34 +0800

    modified:   application/controllers/Login.php

--- application/controllers/Login.php
+++ application/controllers/Login.php
@@ -8,7 +8,7 @@ class Login extends MY_Controller {
     }
 
     public function ajaxLogin() {
-        $admin = array(); 
+        $admin = array();  
         if($this->input->post('admin_name') && $this->input->post('admin_pwd')) {
             $admin_name = $this->input->post('admin_name');
             $admin_pwd = $this->input->post('admin_pwd');
@@ -44,8 +44,7 @@ class Login extends MY_Controller {
                 'admin_name' => $admin['admin_name'],
                 'expires_time' => date('Y-m-d H:i:s', time() + 60*60)
             );
-            $this->session_token_model->setSession($data);
-
+            $this->session_token_model->setSession($data); 
             set_cookie('login_sessiontoken', session_id(), 60*60*24);
             set_cookie('login_username', $admin['admin_name'], 60*60*24);
 
